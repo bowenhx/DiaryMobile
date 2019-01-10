4399d993d0ae9e0b6809076f2cc07afc351ab683
Stray <ligbhx@163.com>
2019-01-10 17:25:16 +0800

    添加好友日記頁面

---
+++ DiaryMobile/FriendVC/FriendsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FriendsTableViewController.h
+//  DiaryMobile
+//
+//  Created by ligb on 2019/1/10.
+//  Copyright © 2019 com.professional.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FriendsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DiaryMobile/FriendVC/FriendsTableViewController.m
@@ -0,0 +1,155 @@
+//
+//  FriendsTableViewController.m
+//  DiaryMobile
+//
+//  Created by ligb on 2019/1/10.
+//  Copyright © 2019 com.professional.cn. All rights reserved.
+//
+
+#import "FriendsTableViewController.h"
+#import "KDefine.h"
+#import "BlogListCell.h"
+#import "LogListModel.h"
+#import "WebViewController.h"
+
+static NSString *kBLOG_CELL = @"BlogListCell";
+
+@interface FriendsTableViewController ()
+
+//好友日志，数据源
+@property (nonatomic , strong) NSMutableArray *dataSource;
+//临时数据
+@property (nonatomic , strong) NSMutableArray <LogListModel *>* tempData;
+
+//我的日志，好友日志，页码
+@property (nonatomic , assign) NSInteger vPage;
+
+//Yes代表下拉刷新，No代表上啦加载更多
+@property (nonatomic , assign) BOOL isPullRefresh;
+@end
+
+@implementation FriendsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.title = @"好友日記";
+    
+    [self loadNewView];
+}
+
+- (void)loadNewView {
+    _dataSource = [NSMutableArray array];
+    _tempData = [NSMutableArray array];
+    //注册xib
+    [self.tableView registerNib:[UINib nibWithNibName:kBLOG_CELL bundle:nil] forCellReuseIdentifier:kBLOG_CELL];
+    //添加下拉刷新功能
+    MJRefreshGifHeader* gifHeader = [MJRefreshGifHeader headerWithRefreshingTarget:self refreshingAction:@selector(pullLoadAction)];
+    gifHeader.lastUpdatedTimeLabel.hidden = YES;
+    self.tableView.mj_header = gifHeader;
+    [self.tableView.mj_header beginRefreshing];
+    
+    //添加上拉加载更多功能
+    MJRefreshBackGifFooter* gifFooter = [MJRefreshBackGifFooter footerWithRefreshingTarget:self refreshingAction:@selector(uploadingAction)];
+    self.tableView.mj_footer = gifFooter;
+    self.tableView.mj_footer.hidden = YES;
+
+}
+
+
+//下拉刷新
+- (void)pullLoadAction {
+    _isPullRefresh = YES;
+    _vPage--;
+    if (_vPage < 1 ) {
+        _vPage = 1;
+    }
+    [self.tableView.mj_footer resetNoMoreData];
+    [self refreshMyTableView];
+}
+
+//上啦加载
+- (void)uploadingAction {
+    _isPullRefresh = NO;
+    _vPage ++;
+    [self refreshMyTableView];
+}
+
+- (void)refreshMyTableView {
+    [self refreshMyFriendBlogData];
+}
+
+//好友日志
+- (void)refreshMyFriendBlogData {
+    [self.view showHUDActivityView:@"正在加載..." shade:NO];
+    [LogListModel loadMyFriendBlogListData:_vPage block:^(NSArray *data, LogListPage *page, NSString *netErr) {
+        [self.view removeHUDActivity];
+        
+        [self.tableView endRefreshing];
+        if (netErr) {
+            [self.view showHUDTitleView:netErr image:nil];
+        } else {
+            [self.tempData setArray:data];
+            [self uniteLoadData];
+            
+//            //表滚动逻辑
+//            if (_isPullRefresh && _vPage != 1) {
+//                [self mTableScrollToBottom];
+//            } else {
+//                [self mTableScrollToTop];
+//            }
+        }
+        
+        if ([netErr hasPrefix:@"沒有更多"] || page.max_page == _vPage) {
+            [self.tableView.mj_footer endRefreshingWithNoMoreData];
+        }
+    }];
+}
+
+- (void)uniteLoadData {
+    [self.dataSource setArray:self.tempData];
+    [self.tableView reloadData];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    tableView.mj_footer.hidden = self.dataSource.count ? NO : YES;
+    return _dataSource.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    BlogListCell *cell = [tableView dequeueReusableCellWithIdentifier:kBLOG_CELL forIndexPath:indexPath];
+   
+    LogListModel *model = _dataSource[indexPath.row];
+    [cell mRefreshBlogCell:model type:EKBlogCellTypeWithNormalBlog];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    LogListModel *model = _dataSource[indexPath.row];
+    if (model.blogid == 0) {
+        return 50;
+    }
+    return model.height + 55;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    LogListModel *model = _dataSource[indexPath.row];
+    WebViewController *webVC = [[WebViewController alloc] initWithNibName:@"WebViewController" bundle:nil];
+    webVC.vNavTitle = @"日誌";
+    webVC.name = model.subject;
+    webVC.ispassword = model.ispassword;
+    webVC.uid = string(model.uid);
+    webVC.tid = string(model.blogid);
+    [self.navigationController pushViewController:webVC animated:YES];
+
+}
+
+@end
